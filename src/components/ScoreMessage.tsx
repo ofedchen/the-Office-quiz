03a9f7fbe9c2c25f5d0dfa70916a79d9eb9956c0
Olha Fedchenko <ofedchen@gmail.com>
2025-11-22 19:57:42 +0100

    add scoreMessage component

---
+++ src/components/ScoreMessage.tsx
@@ -0,0 +1,39 @@
+type Props = {
+  score: number;
+};
+
+type scoreScale = "0-2" | "3-5" | "6-9" | "10+";
+
+type Message = `${scoreScale}: ${string}`;
+
+export default function ScoreMessage({ score }: Props) {
+  let message: Message;
+  let gifOption: string;
+  if (score < 3) {
+    message =
+      "0-2: Yikes… looks like all you know about the Office is from occasional gifs. Just watch the show!";
+    gifOption = "loser.gif";
+  } else if (score < 6) {
+    message =
+      "3-5: You're basically Creed: confused, but sometimes getting things right. Cool beans!";
+    gifOption = "ok.gif";
+  } else if (score < 10) {
+    message =
+      "6-9: Not bad at all! You could use a rewatch round or two, but your vibe is perfect";
+    gifOption = "great.gif";
+  } else {
+    message =
+      "10+: Legendary! You're the new Assistant Regional Manager. Sorry - Assistant to the Regional Manager.";
+    gifOption = "legend.gif";
+  }
+
+  return (
+    <section className="results">
+      <div className="text-container">
+        <h2>Your score: {score}</h2>
+        <p>{message}</p>
+      </div>
+      <img src={gifOption} alt="final-gif" />
+    </section>
+  );
+}
