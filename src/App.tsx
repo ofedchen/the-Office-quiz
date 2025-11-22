b4388318536901453f7ebaf8bc4f84987da03dd1
Olha Fedchenko <ofedchen@gmail.com>
2025-11-22 20:00:29 +0100

    implement app logic and add styling

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { questions } from "./questions";
+import "./App.css";
+import QuestionRenderer from "./components/QuestionRenderer";
+import ScoreMessage from "./components/ScoreMessage";
+
+function App() {
+  const [score, setScore] = useState<number>(0);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [showResults, setShowResults] = useState<boolean>(false);
+  const [isLast, setIsLast] = useState<boolean>(false);
+
+  function handleAnswer(isCorrect: boolean): void {
+    if (isCorrect) {
+      setScore((prev) => prev + 1);
+    }
+
+    if (currentIndex < questions.length - 1) {
+      setCurrentIndex((prev) => prev + 1);
+      if (currentIndex === questions.length - 2) setIsLast(true);
+    } else {
+      setShowResults(true);
+    }
+  }
+
+  return (
+    <>
+      <header>
+        <h1>Your ultimate The Office quiz</h1>
+      </header>
+      <main>
+        {!showResults ? (
+          <QuestionRenderer
+            q={questions[currentIndex]}
+            onSubmit={handleAnswer}
+            {...(isLast && { last: isLast })}
+          />
+        ) : (
+          <ScoreMessage score={score} />
+        )}
+      </main>
+    </>
+  );
+}
+
+export default App;
