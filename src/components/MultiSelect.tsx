472d36da325259ae477d69a045e23768f36ecdb4
Olha Fedchenko <ofedchen@gmail.com>
2025-11-22 19:59:32 +0100

    add more question components

---
+++ src/components/MultiSelect.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import type { QuestionStyled } from "../questions";
+
+type Props = {
+  q: QuestionStyled;
+  onSubmit: (isCorrect: boolean) => void;
+  last: boolean;
+};
+
+export default function MultiSelect({ q, onSubmit, last }: Props) {
+  const [selectedAnswers, setSelectedAnswers] = useState<string[]>([]);
+
+  if (q.type !== "multi-select") return null;
+
+  const updateAnswers = (answer: string) => {
+    if (selectedAnswers.includes(answer)) {
+      setSelectedAnswers((prev) => prev.filter((a) => a !== answer));
+    } else {
+      setSelectedAnswers((prev) => [...prev, answer]);
+    }
+  };
+
+  const handleSubmit = () => {
+    if (!selectedAnswers.length) return;
+    
+    const isCorrect = 
+      q.answers.length === selectedAnswers.length &&
+      q.answers.every((answer) => selectedAnswers.includes(answer));
+    
+    onSubmit(isCorrect);
+  };
+
+  return (
+    <>
+      <div className="question-container">
+        <h2>{q.question}</h2>
+        <div className="options">
+          {q.options.map((option) => (
+            <label key={option} className="option">
+              <input
+                type="checkbox"
+                name="multi-select"
+                id={option}
+                value={option}
+                checked={selectedAnswers.includes(option)}
+                onChange={(e) => updateAnswers(e.target.value)}
+              />
+              {option}
+            </label>
+          ))}
+        </div>
+        <img src={q.gif} alt="some kind of the office gif" />
+        <button onClick={handleSubmit} disabled={!selectedAnswers.length}>
+          {!last ? "Next" : "Finish quiz"}
+        </button>
+      </div>
+    </>
+  );
+}
---
+++ src/components/MultipleChoice.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import type { QuestionStyled } from "../questions";
+
+type Props = {
+  q: QuestionStyled;
+  onSubmit: (isCorrect: boolean) => void;
+  last: boolean;
+};
+
+export default function MultipleChoice({ q, onSubmit, last }: Props) {
+  const [selectedAnswer, setSelectedAnswer] = useState<string>("");
+
+  if (q.type !== "multiple-choice") return null;
+
+  const handleSubmit = () => {
+    if (!selectedAnswer) return;
+    const isCorrect = selectedAnswer === q.answer;
+    onSubmit(isCorrect);
+  };
+
+  return (
+    <div className="question-container">
+      <h2>{q.question}</h2>
+      <div className="options">
+        {q.options.map((option) => (
+          <label key={option} className="option radio">
+            <input
+              type="radio"
+              name="multiple-choice"
+              id={option}
+              value={option}
+              checked={selectedAnswer === option}
+              onChange={(e) => setSelectedAnswer(e.target.value)}
+            />
+            {option}
+          </label>
+        ))}
+      </div>
+      <img src={q.gif} alt="some kind of the office gif" />
+      <button onClick={handleSubmit} disabled={!selectedAnswer}>
+        {!last ? "Next" : "Finish quiz"}
+      </button>
+    </div>
+  );
+}
