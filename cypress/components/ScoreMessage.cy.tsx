6236d13a4c2cb8dea6503188d8eccce0a53e2dd5
Olha Fedchenko <ofedchen@gmail.com>
2025-11-28 18:24:22 +0100

    simplified attr syntax

--- cypress/components/ScoreMessage.cy.tsx
+++ cypress/components/ScoreMessage.cy.tsx
@@ -6,43 +6,37 @@ describe("<ScoreMessage />", () => {
     cy.mount(<ScoreMessage finalScore={2} />);
     cy.get("[data-cy='result-gif']")
       .should("be.visible")
-      .should("have.attr", "src")
-      .and("include", "loser.gif");
+      .should("have.attr", "src", "loser.gif");
   });
   it("renders correct gif for score 3-5 with score 3", () => {
     cy.mount(<ScoreMessage finalScore={3} />);
     cy.get("[data-cy='result-gif']")
       .should("be.visible")
-      .should("have.attr", "src")
-      .and("include", "ok.gif");
+      .should("have.attr", "src", "ok.gif");
   });
   it("renders correct gif for score 3-5 with score 5", () => {
     cy.mount(<ScoreMessage finalScore={5} />);
     cy.get("[data-cy='result-gif']")
       .should("be.visible")
-      .should("have.attr", "src")
-      .and("include", "ok.gif");
+      .should("have.attr", "src", "ok.gif");
   });
   it("renders correct gif for score 6-9 with score 6", () => {
     cy.mount(<ScoreMessage finalScore={6} />);
     cy.get("[data-cy='result-gif']")
       .should("be.visible")
-      .should("have.attr", "src")
-      .and("include", "great.gif");
+      .should("have.attr", "src", "great.gif");
   });
   it("renders correct gif for score 6-9 with score 9", () => {
     cy.mount(<ScoreMessage finalScore={9} />);
     cy.get("[data-cy='result-gif']")
       .should("be.visible")
-      .should("have.attr", "src")
-      .and("include", "great.gif");
+      .should("have.attr", "src", "great.gif");
   });
   it("renders correct gif for score 10-12 with score 10", () => {
     cy.mount(<ScoreMessage finalScore={10} />);
     cy.get("[data-cy='result-gif']")
       .should("be.visible")
-      .should("have.attr", "src")
-      .and("include", "legend.gif");
+      .should("have.attr", "src", "legend.gif");
   });
 
   it("renders correct message for score 0-2 with score 2", () => {
